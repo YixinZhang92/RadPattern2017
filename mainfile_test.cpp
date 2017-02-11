46a68ced3212da08c295d665940b0244055183ab
YixinZhang92 <yzhang22@memphis.edu>
2017-02-11 13:25:18 -0600

    Create mainfile_test.cpp

---
+++ mainfile_test.cpp
@@ -0,0 +1,23 @@
+
+/*
+ * Initial main file example
+ * by Yixin Zhang
+ */
+ 
+#inculde <iostream>
+
+using namespace std;
+
+int main() {
+
+  // Print out of program message
+  
+  cout << "\n"
+          "Hello,\n"
+          "I am your first test program\n";
+  cout << endl;
+  
+  return 0;
+  
+}
+ 
