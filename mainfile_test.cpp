56aff725d47e8e2fea1dd060c96ad72441f46ca0
Oluwaseun Fadugba <oluwaseun_fadugba@yahoo.com>
2017-02-14 03:33:09 -0500

    Oluwaseun added the prototypes of the displacements functions for all force and wave types, and the write-to-file functions (2/14/2017)

--- mainfile_test.cpp
+++ mainfile_test.cpp
@@ -3,21 +3,318 @@
  * Initial main file example
  * by Yixin Zhang
  */
- 
-#inculde <iostream>
+
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
 
 using namespace std;
 
-int main() {
+int main()
+{
 
   // Print out of program message
-  
+
   cout << "\n"
           "Hello,\n"
           "I am your first test program\n";
   cout << endl;
-  
+
   return 0;
-  
+
 }
- 
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * displacement_point_force_P_wave
+ * by Oluwaseun Fadugba
+ * February 14, 2017
+* 
+ * This function calculates the P-wave Displacement for single force using the values of theta, phi,  distance (R), moment (C1), P-wave velocity (alpha), density (rho), the input waveform (h) and the time series (time).
+ */
+
+double displacement_point_force_P_wave (double theta, double R, double moment, double alpha, double rho, double h[], double time[])
+{
+
+cout <<  "Running: displacement_point_force_P_wave\n";
+cout << endl;
+
+return 0;
+
+}
+
+/*
+ * displacement_point_force_S_wave
+ * by Oluwaseun Fadugba
+ * February 14, 2017
+* 
+ * This function calculates the S-wave Displacement for single force using the values of theta, phi,  distance (R), moment (C1), S-wave velocity (beta), density (rho), the input waveform (h) and the time series (time).
+ */
+
+double displacement_point_force_S_wave (double theta, double phi, double R, double moment, double beta, double rho, double h[], double time[])
+{
+
+cout <<  "Running: displacement_point_force_S_wave\n";
+cout << endl;
+return 0;
+
+}
+
+
+/*
+ * displacement_single_force_P_wave
+ * by Oluwaseun Fadugba
+ * February 14, 2017
+* 
+ * This function calculates the P-wave Displacement for single force using the values of theta, phi,  distance (R), moment (C1), P-wave velocity (alpha), density (rho), derivative of the input waveform (h) and derivative of the time series (time).
+ */
+
+double displacement_single_force_P_wave (double theta, double phi, double R, double moment, double alpha, double rho, double h_derivative[], double time_derivative[])
+{
+
+cout <<  "Running: displacement_single_force_P_wave\n";
+cout << endl;
+
+return 0;
+
+}
+
+
+/*
+ * displacement_single_force_SH_wave
+ * by Oluwaseun Fadugba
+ * February 14, 2017
+* 
+ * This function calculates the SH-wave Displacement for single force using the values of theta, phi,  distance (R), moment (C1), S-wave velocity (beta), density (rho), derivative of the input waveform (h) and derivative of the time series (time).
+ */
+
+double displacement_single_force_SH_wave (double theta, double phi, double R, double moment, double beta, double rho, double h_derivative[], double time_derivative[])
+{
+
+cout <<  "Running: displacement_single_force_SH_wave\n";
+cout << endl;
+
+
+return 0;
+
+}
+
+
+
+/*
+ * displacement_single_force_SV_wave
+ * by Oluwaseun Fadugba
+ * February 14, 2017
+* 
+ * This function calculates the SV-wave Displacement for single force using the values of theta, phi,  distance (R), moment (C1), S-wave velocity (beta), density (rho), derivative of the input waveform (h) and derivative of the time series (time).
+ */
+
+double displacement_single_force_SV_wave (double theta, double phi, double R, double moment, double beta, double rho, double h_derivative[], double time_derivative[])
+{
+
+cout <<  "Running: displacement_single_force_SV_wave\n";
+cout << endl;
+
+
+return 0;
+
+}
+
+
+
+
+/*
+ * displacement_double_couple_P_wave
+ * by Oluwaseun Fadugba
+ * February 14, 2017
+* 
+ * This function calculates the P-wave Displacement for Double Couple using the values of theta, phi,  distance (R), moment (C1), P-wave velocity (alpha), density (rho), derivative of the input waveform (h) and derivative of the time series (time).
+ */
+
+double displacement_double_couple_P_wave (double theta, double phi, double R, double moment, double alpha, double rho, double h_derivative[], double time_derivative[])
+{
+
+cout <<  "Running: displacement_double_couple_P_wave\n";
+cout << endl;
+
+return 0;
+
+}
+
+
+/*
+ * displacement_double_couple_SH_wave
+ * by Oluwaseun Fadugba
+ * February 14, 2017
+ *
+ * This function calculates the SH-wave Displacement for Double Couple using the values of theta, phi,  distance (R), moment (C1), S-wave velocity (beta), density (rho), derivative of the input waveform (h) and derivative of the time series (time).
+ */
+
+double displacement_double_couple_SH_wave (double theta, double phi, double R, double moment, double beta, double rho, double h_derivative[], double time_derivative[])
+{
+
+cout <<  "Running: displacement_double_couple_SH_wave\n";
+cout << endl;
+
+return 0;
+
+}
+
+
+
+/*
+ * displacement_double_couple_SV_wave
+ * by Oluwaseun Fadugba
+ * February 14, 2017
+ *
+ * This function calculates the SV-wave Displacement for Double Couple using the values of theta, phi,  distance (R), moment (C1), S-wave velocity (beta), density (rho), derivative of the input waveform (h) and derivative of the time series (time).
+ */
+
+double displacement_double_couple_SV_wave (double theta, double phi, double R, double moment, double beta, double rho, double h_derivative[], double time_derivative[])
+{
+
+cout <<  "Running: displacement_double_couple_SV_wave\n";
+cout << endl;
+
+return 0;
+
+}
+
+
+
+/*
+ * displacement_force_dipole_P_wave
+ * by Oluwaseun Fadugba
+ * February 14, 2017
+ *
+ * This function calculates the P-wave Displacement for Force Dipole using the values of theta, phi,  distance (R), moment (C1), P-wave velocity (alpha), density (rho), input waveform (h) and time series (time).
+ */
+
+double displacement_force_dipole_P_wave (double theta, double phi, double R, double moment, double alpha, double rho, double h[], double time[])
+{
+
+cout <<  "Running: displacement_force_dipole_P_wave\n";
+cout << endl;
+
+return 0;
+
+}
+
+
+/*
+ * displacement_force_dipole_SH_wave
+ * by Oluwaseun Fadugba
+ * February 14, 2017
+ *
+ * This function calculates the SH-wave Displacement for Force Dipole using the values of theta, phi,  distance (R), moment (C1), S-wave velocity, density (rho), input waveform (h) and time series (time).
+*/
+
+double displacement_force_dipole_SH_wave (double theta, double phi, double R, double moment, double beta, double rho, double h[], double time[])
+{
+
+cout <<  "Running: displacement_force_dipole_SH_wave\n";
+cout << endl;
+
+return 0;
+
+}
+
+
+/*
+ * displacement_force_dipole_SV_wave
+ * by Oluwaseun Fadugba
+ * February 14, 2017
+ *
+ * This function calculates the SV-wave Displacement for Force Dipole using the values of theta, phi,  distance (R), moment (C1), S-wave velocity, density (rho), input waveform (h) and time series (time).
+ */
+
+double displacement_force_dipole_SV_wave (double theta, double phi, double R, double moment, double beta, double rho, double h[], double time[])
+{
+
+cout <<  "Running: displacement_force_dipole_SV_wave\n";
+cout << endl;
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+/*
+ * write_P_waves_to_file
+ * by Oluwaseun Fadugba
+ * February 14, 2017
+ * This function writes the all the P-wave displacements, input waveform and its derivative and its location (x,y) into a file
+ */
+
+int write_P_waves_to_file (double displacement_point_force_P_wave[], double displacement_single_couple_P_wave[], double displacement_double_couple_P_wave[], double displacement_force_dipole_P_wave[], double h[], double h_derivative[], double time[], double time_derivative[], double x[], double y[], string outputfilename)
+{
+
+cout <<  "Running: write_P_waves_to_file\n";
+cout << endl;
+
+return 0;
+
+}
+
+
+
+/*
+ * write_SV_waves_to_file
+ * by Oluwaseun Fadugba
+ * February 14, 2017
+ * This function writes the all the SV-wave displacements, input waveform and its derivative and its location (x,y) into a file
+ */
+
+int write_SV_waves_to_file (double displacement_point_force_S_wave[], double displacement_single_couple_SV_wave[], double displacement_double_couple_SV_wave[], double displacement_force_dipole_SV_wave[], double h[], double h_derivative[], double time[], double time_derivative[], double x[], double y[], string outputfilename)
+{
+
+cout <<  "Running: write_SV_waves_to_file\n";
+cout << endl;
+
+return 0;
+
+}
+
+
+
+/*
+ * write_SH_waves_to_file
+ * by Oluwaseun Fadugba
+ * February 14, 2017
+ * This function writes the all the SH-wave displacements, input waveform and its derivative and its location (x,y) into a file
+ */
+
+int write_SH_waves_to_file (double displacement_point_force_S_wave[], double displacement_single_couple_SH_wave[], double displacement_double_couple_SH_wave[], double displacement_force_dipole_SH_wave[], double h[], double h_derivative[], double time[], double time_derivative[], double x[], double y[], string outputfilename)
+{
+
+cout <<  "Running: write_SH_waves_to_file\n";
+cout << endl;
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
