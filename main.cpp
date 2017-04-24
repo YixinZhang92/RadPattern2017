ab31f79c6cce30d7f54f88522035b9ab89be5aaf
Oluwaseun Fadugba <oluwaseun_fadugba@yahoo.com>
2017-04-24 15:05:16 -0400

    MPI is running but have some bugs in the output files

--- main.cpp
+++ main.cpp
@@ -30,6 +30,7 @@
 #include <cstdlib>
 #include <cmath>
 #include <time.h>
+#include <mpi.h>
 
 #include "init_time.h"
 #include "struct.h"
@@ -58,15 +59,57 @@ int main(int argc, char* argv[])
 {  
     clock_t t1 = clock(); //beginning time
 
+    int rank;
+    int size;
+
+    // Initialize an MPI process
+    // Passing the arguments of the main program
+    MPI_Init(&argc, &argv);
+
+    // Captures the rank (i.e., the processor ID)
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    // Captures the total number of processes
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (size != 4 ) 
+    {
+        cout << "\nQuitting. Number of MPI tasks must be 4.\n\n";
+        MPI_Abort(MPI_COMM_WORLD, 0);
+        exit(EXIT_FAILURE);
+    };
+
+    if ( rank == 0 ) 
+    {
+        cout << "Welcome to Radiation Pattern parallel program!\n";
+        cout << endl;
+        
+    }; 
+
+
+    // Create a barrier here
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Introduction by each processors
+    cout << "I am processor " << rank << " out of " << size << endl;
+  
+    // Create a barrier here
+    MPI_Barrier(MPI_COMM_WORLD);
+
+
+    
     // Declare all parameters and files
     Parameters params;
     displacement displ; 
     radiation_pattern radiation;
     
-    // Parameters will be read from input file, checked  for their reasonability,
-    // stored into memory, and then written into login file
-    process_parameter(argc, argv, &params);
-      
+   // if ( rank == 1 ) 
+    //{
+        // Parameters will be read from input file, checked  for their reasonability,
+        // stored into memory, and then written into login file
+        process_parameter(argc, argv, &params); 
+    //};
+
     int len = params.total_time/ params.time_step;
     double *t = new double[len];   
     init_time (t, len, &params); //initializes time array
@@ -77,8 +120,14 @@ int main(int argc, char* argv[])
     double *theta = new double[1];
     double *phi = new double[1]; 
 
-    // This function generates a guassian function and its derivative
-    gauss_func (h, h_der, len, &params);
+    // Create a barrier here
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (rank == 1 ) 
+    {
+        // This function generates a guassian function and its derivative
+        gauss_func (h, h_der, len, &params);
+    };
 
     // Now we want to iterate over the grid centers and determine the radiation
     // pattern and displacement based on the type of force specified
@@ -94,15 +143,49 @@ int main(int argc, char* argv[])
 
             check_grid(xx, yy, &params);
             cart_2_sph (xx, yy, R, theta, phi);
-            compute_displ (R[1], theta[1], phi[1] , h, h_der, &displ, len, &params);
-            rad_pattern (theta[1], phi[1], &radiation, &params);
-            write_2_file (&displ, &radiation, t, xx, yy, params.outputfile_path, len);
+
+            if ((xx < 0) & (yy > 0) & (rank == 0))
+            {
+                compute_displ (R[1], theta[1], phi[1] , h, h_der, &displ, len, &params);
+                rad_pattern (theta[1], phi[1], &radiation, &params);
+                write_2_file (&displ, &radiation, t, xx, yy, params.outputfile_path, len);
+            };
+
+            if ((xx >= 0) & (yy >= 0) & (rank == 1))
+            {
+                compute_displ (R[1], theta[1], phi[1] , h, h_der, &displ, len, &params);
+                rad_pattern (theta[1], phi[1], &radiation, &params);
+                write_2_file (&displ, &radiation, t, xx, yy, params.outputfile_path, len);
+            };
+
+            if ((xx > 0) & (yy < 0) & (rank == 2))
+            {
+                compute_displ (R[1], theta[1], phi[1] , h, h_der, &displ, len, &params);
+                rad_pattern (theta[1], phi[1], &radiation, &params);
+                write_2_file (&displ, &radiation, t, xx, yy, params.outputfile_path, len);
+            };
+
+            if ((xx <= 0) & (yy <= 0) & (rank == 3))
+            {
+                compute_displ (R[1], theta[1], phi[1] , h, h_der, &displ, len, &params);
+                rad_pattern (theta[1], phi[1], &radiation, &params);
+                write_2_file (&displ, &radiation, t, xx, yy, params.outputfile_path, len);
+            };
+
+            // Create a barrier here
+            MPI_Barrier(MPI_COMM_WORLD);
+
         };
     };
 
-    cout << "I have completed running all the functions.\n"
-    "Good bye.\n"<< endl;
-    std::cout << "Execution time : "<<float( clock () - t1 ) / CLOCKS_PER_SEC<< endl;
+    if ( rank == 0 ) 
+    {    
+        cout << "I have completed running all the functions.\n"
+        "Good bye.\n"<< endl;
+        std::cout << "Execution time : "<<float( clock () - t1 ) / CLOCKS_PER_SEC<< endl;
+    };
+    
+    MPI_Finalize();
 
     return 0;
 }
